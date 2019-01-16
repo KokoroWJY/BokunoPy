a87dcfc68a63caa2f4a91c8287989cee1f9207bf
YOur Name <you@example.com>
2019-01-16 19:15:38 +0800

    My learn

--- ClassMooc/Game.py
+++ ClassMooc/Game.py
@@ -1,4 +1,6 @@
 #测试
-favorite_language = "python "
+favorite_language = " python "
 print(favorite_language)
-print(favorite_language.rstrip())
+print(favorite_language.rstrip()) # rstrip 去掉最后的空格
+print(favorite_language.lstrip()) # lstrip 去掉前面的空格
+print(favorite_language.strip()) #去掉两端空格
