15bd49306eaa6aa5531874b4f1a62b86158429fa
YOur Name <you@example.com>
2019-01-17 22:05:53 +0800

    My learn .py

---
+++ ClassMooc/WjyOne.py
@@ -0,0 +1,29 @@
+a = 3 * 3
+b = 3 ** 3
+print(a)
+print(b)
+c = pow(3,3)
+print(c)
+print(0.2 + 0.1)
+print(3 * 0.1)
+
+age = 19
+print("Happy " + str(age) + "rd birthday!") # str转化为字符串
+
+print(3 / 2)
+
+like = input("你喜欢的一个数 :")
+message = "Your like number is " + str(like)
+print(message)
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+del bicycles[0] # del删除某一列表的某位元素
+print(bicycles)
+
+names = ["张中钰", "魏俊宇", "王金龙"]
+print("\n" + names[0] + names[1] + names[2])
+
+print("\n" + names[-1] + "新年快乐!!")
+print(names[-2] + "新年快乐!!")
+print(names[-3] + "新年快乐!!")
