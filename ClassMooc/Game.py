2992060d5febbfc53de2ebd300658dd7e5b216ed
YOur Name <you@example.com>
2019-01-21 09:04:30 +0800

    My learn .py

--- ClassMooc/Game.py
+++ ClassMooc/Game.py
@@ -1,8 +1,8 @@
 #测试
-favorite_language = " python "
+favorite_language = "  python  "
 print(favorite_language)
-print(favorite_language.rstrip()) # rstrip 去掉最后的空格
-print(favorite_language.lstrip()) # lstrip 去掉前面的空格
+print(favorite_language.rstrip()) # rstrip 去掉最后的空白
+print(favorite_language.lstrip()) # lstrip 去掉前面的空白
 print(favorite_language.strip()) #去掉两端空格
 
 name = input("Plase tell me your name :")
