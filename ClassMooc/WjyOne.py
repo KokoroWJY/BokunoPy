acd2467f9b05fe0c0d608abbb1fd88be0e6512e9
Your Name <email@example.com>
2019-01-29 17:11:28 +0800

    My learn .py

--- ClassMooc/WjyOne.py
+++ ClassMooc/WjyOne.py
@@ -2,13 +2,13 @@ a = 3 * 3
 b = 3 ** 3
 print(a)
 print(b)
-c = pow(3,3)
+c = pow(3, 3)
 print(c)
 print(0.2 + 0.1)
 print(3 * 0.1)
 
 age = 19
-print("Happy " + str(age) + "rd birthday!") # str转化为字符串
+print("Happy " + str(age) + "rd birthday!")  # str转化为字符串
 
 print(3 / 2)
 
@@ -18,7 +18,7 @@ print(message)
 
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
-del bicycles[0] # del删除某一列表的某位元素
+del bicycles[0]  # del删除某一列表的某位元素
 print(bicycles)
 
 names = ["张", "魏", "王"]
@@ -30,21 +30,21 @@ print(names[-3] + "新年快乐!!")
 
 motorcycles = ["honda", "yamaha", "suzuki"]
 print(motorcycles)
-poped_motorcycles = motorcycles.pop() # pop() 删除列表中的某一元素 但依旧保留它的值
+poped_motorcycles = motorcycles.pop()  # pop() 删除列表中的某一元素 但依旧保留它的值
 print(motorcycles)
 print(poped_motorcycles)
 
 motorcycles1 = ["honda", "yamaha", "suzuki", "ducati"]
 print(motorcycles1)
-motorcycles1.remove("ducati") # remove 删除列表中某一元素 你不用知道此元素的位置 Python会自己找到并删除它
+motorcycles1.remove("ducati")  # remove 删除列表中某一元素 你不用知道此元素的位置 Python会自己找到并删除它
 print(motorcycles1)
 
-guest = ["李","陈","庞","郭"]
-guest.insert(0, "陶") # insert() 在列表中的某一位置添加元素 只用输入两个参数即可
+guest = ["李", "陈", "庞", "郭"]
+guest.insert(0, "陶")  # insert() 在列表中的某一位置添加元素 只用输入两个参数即可
 print(guest)
 guest.insert(2, "朱")
 print(guest)
-guest.append("徐") # append() 在列表最后添加一个元素 只用输入一个参数即可
+guest.append("徐")  # append() 在列表最后添加一个元素 只用输入一个参数即可
 print(guest)
 for i in range(4):
     print("在此邀请" + guest[i] + "先生/女士, 参加本次的宴会")
@@ -53,7 +53,7 @@ local = ["Japen", "England", "China", "Ganada", "Germany"]
 print(local)
 print(sorted(local))
 print(local)
-print (sorted(local, reverse=True))
+print(sorted(local, reverse=True))
 print(local)
 local.reverse()
 print(local)
@@ -83,17 +83,17 @@ print("Any of these animals would make a great pet!")
 
 listOne = list(range(2, 10, 2))
 print(listOne)
-print(sum(listOne)) # 对listOne求和
-print(max(listOne)) # 找出listOne最大值
-print(min(listOne)) # 找出listOne最小值
+print(sum(listOne))  # 对listOne求和
+print(max(listOne))  # 找出listOne最大值
+print(min(listOne))  # 找出listOne最小值
 
-squares = [value**2 for value in range(1,11)]
+squares = [value ** 2 for value in range(1, 11)]
 print(squares)
 
-for a in range(1,21):
+for a in range(1, 21):
     print(a, end=" ")
 
-listOne = list(range(1,1000001))
+listOne = list(range(1, 1000001))
 for b in range(0, 100000):
     print(listOne[b], end=" ")
     if b % 10 == 0:
@@ -101,8 +101,8 @@ for b in range(0, 100000):
 print('\n最小的数是: ' + str(min(listOne)))
 print('最大的数是: ' + str(max(listOne)))
 
-listTwo = list(range(1,21,2))
-for Two in range(0,len(listTwo)):
+listTwo = list(range(1, 21, 2))
+for Two in range(0, len(listTwo)):
     print(listTwo[Two], end=' ')
 
 listThree = []
@@ -111,11 +111,11 @@ while a <= 30:
     if a % 3 == 0:
         listThree.append(a)
     a = a + 1
-for Three in range(0,len(listThree)):
+for Three in range(0, len(listThree)):
     print(listThree[Three], end='\t')
 
 print()
 
-listFour = [four**3 for four in range(0, 11)]
+listFour = [four ** 3 for four in range(0, 11)]
 for Four in range(0, len(listFour)):
     print(listFour[Four], end="\t")
---
+++ ClassMooc/WjyTwo.py
@@ -0,0 +1,34 @@
+# 列表切片
+players = ['charles', 'martina', 'minchael', 'florence', 'eli']
+print(players[0:3])
+
+# 遍历切片
+players1 = ['charles', 'martina', 'minchael', 'florence', 'eli']
+print('Here ate the first three players on my team: ', end='\t')
+for player in players1[:3]:
+    print(player.title(), end='\t')
+
+# 复制列表
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+print('My favorite foods are: ' + str(my_foods))
+print("My friend's favorite foods are: " + str(friend_foods))
+
+# 习题
+players2 = ['charles', 'martina', 'minchael', 'florence', 'eli']
+print("The first three items in the list are:")
+print(players2[:3])
+print("Three items from the middle of the list are:")
+print(players2[1:-2])
+print("The last three items in the list are:")
+print(players2[-3:])
+
+my_pizzas = ['apple', 'falafel', 'carrot cake']
+friend_pizzas = my_pizzas[:]
+friend_pizzas.append('ice cream')
+print('My favourte pizzas are:')
+for my_pizza in my_pizzas:
+    print(my_pizza, end=' ')
+print('Friend favourite pizzas are:')
+for friend_pizza in  friend_pizzas:
+    print(friend_pizza, end=' ')
