3622f17928c9f8fa79dde78cccae074c182c061e
KokoroWJY <oumainoli@gmail.com>
2019-02-05 13:34:30 +0800

    My learn .py

--- ClassMooc/WjyThree.py
+++ ClassMooc/WjyThree.py
@@ -68,3 +68,48 @@ for a in range(0, 7):
         print('感谢' + favorite_languagees[a] + '您的到来')
     else:
         print('希望' + favorite_languagees[a] + '您能参加')
+
+# 嵌套
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+aliens = [alien_0, alien_1, alien_2]
+for alien in aliens:
+    print(alien)
+
+# 创建一个储存外星人的空列表
+aliens = []
+# 创建30个绿色的外星人
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+# 显示前5个外星人
+for alien in aliens[:5]:
+    print(alien)
+print("......")
+# 显示创建了几个外星人
+print("Total number of aliens: " + str(len(aliens)))
+
+# 在字典中储存列表
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese']
+}
+print("You ordered a " + pizza['crust'[1]] + "-crust pizza")
+
+# 在字典中储存字典
+users = {
+    'aeinstein': {
+        'first': 'albert', 'last': 'en=instein', 'location': 'princetion'
+    },
+    'mcurjie': {
+        'first': 'marie', 'last': 'curie', 'location': "paris"
+    }
+}
+for username, user_info in users.items():
+    print("\nUsername: " + username)
+    full_name = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+
+    print("\tFull name: " + full_name.title())
+    print("\tLocation: " + location.title())
