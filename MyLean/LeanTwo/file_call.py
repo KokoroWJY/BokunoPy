1329f195d7418fd3cdfc1e07bb00e0bc5bf7334a
KokoroWJY <oumainoli@gmail.com>
2019-03-01 21:50:44 +0800

    My learn .py

--- MyLean/LeanTwo/file_call.py
+++ MyLean/LeanTwo/file_call.py
@@ -4,6 +4,7 @@ try:
         print("cat_name.txt 文件的内容是: ")
         print(neko)
     with open('dog_name.txt', 'r', encoding='UTF-8') as dog:
+        # emcoding= 将所有的转变为UTF-8的模式
         inu = dog.read()
         print("\ndog_name.txt 文件的内容是: ")
         print(inu)
