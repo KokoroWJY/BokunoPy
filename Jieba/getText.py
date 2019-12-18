1d24e3ced7cf4fdc7a7140ebb614e73cb805934d
KokoroWJY <oumainoli@gmail.com>
2019-12-18 08:21:32 +0800

    My learn .py

--- Jieba/getText.py
+++ Jieba/getText.py
@@ -1,12 +1,13 @@
-import jieba
-
 # 给Hamet(英文版 哈姆雷特)分词
+
 def getText():
+    """全文变为小写并将特殊符号变为空格 以空格分割"""
     txt = open("Hamet.txt", 'r').read()
-    txt = txt.lower() # 所有字母变为小写 避免大小写的影响
-    for ch in 'i"#$%&()+,-/;:<=>?[]@^"*\|~`' + "'":
-        txt = txt.replace(ch, ' ')
+    txt = txt.lower()  # 所有字母变为小写 避免大小写的影响
+    for ch in '!"#$%&()+,-/;:<=>?[]@^"*\|~`' + "'":
+        txt = txt.replace(ch, " ")
     return txt
+
 
 hamletTxt = getText()
 worlds = hamletTxt.split()
@@ -16,3 +17,11 @@ counts = {}
 for world in worlds:
     # counts[world] 检查world这个键
     counts[world] = counts.get(world, 0) + 1
+
+items = list(counts.items())
+""" lambda 用来指定列表中使用那一个多元选择的列作为排序列 默认:从小到大
+reverse=True 返回顺序从大到小"""
+items.sort(key=lambda x: x[1], reverse=True)
+for i in range(10):
+    world, counts = items[i]
+    print("{0:<10}{1:>5}".format(world, counts))
