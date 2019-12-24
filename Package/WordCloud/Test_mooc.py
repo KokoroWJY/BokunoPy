ae852b40e6d82b236f978cdf8566bfe0a3585a4f
KokoroWJY <oumainoli@gmail.com>
2019-12-24 14:55:29 +0800

    My learn .py

--- Package/WordCloud/Test_mooc.py
+++ Package/WordCloud/Test_mooc.py
@@ -1,1 +1,10 @@
 import wordcloud
+import random
+
+red = random.randint(0, 255)
+green = random.randint(0,255)
+blue = random.randint(0, 255)
+txt = "life is short, you need python"
+w = wordcloud.WordCloud(background_color=(red, green, blue))
+w.generate(txt)
+w.to_file("name.jpg")
