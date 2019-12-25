3f597bf04b904777368d9fd112fb676f426a3d61
KokoroWJY <oumainoli@gmail.com>
2019-12-25 08:26:02 +0800

    My learn .py

--- Package/WordCloud/Test_mooc.py
+++ Package/WordCloud/Test_mooc.py
@@ -5,6 +5,6 @@ red = random.randint(0, 255)
 green = random.randint(0,255)
 blue = random.randint(0, 255)
 txt = "life is short, you need python"
-w = wordcloud.WordCloud(background_color=(red, green, blue))
+w = wordcloud.WordCloud()
 w.generate(txt)
-w.to_file("name.jpg")
+w.to_file("name.png")
---
+++ Package/WordCloud/join.py
@@ -0,0 +1,5 @@
+list1 = ['g','e','e','k', 's']
+print("".join(list1))
+
+list2 = ['1', '2', '3', '4']
+print("-".join(list2))
