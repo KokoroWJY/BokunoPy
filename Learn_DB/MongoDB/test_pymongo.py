f0fe6962fc3dca409a511b3b8ab25f4d78086b94
KokoroWJY <oumainoli@gmail.com>
2020-03-04 11:12:15 +0800

    Learn MongoDB -- MongoDB-插入and查询 数据

--- Learn_DB/MongoDB/test_pymongo.py
+++ Learn_DB/MongoDB/test_pymongo.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from datetime import datetime
+from bson.objectid import ObjectId
 
 
 class TestMongo(object):
@@ -16,11 +17,33 @@ class TestMongo(object):
         }
         return self.db.blog.insert_one(post)
 
+    def get_one(self):
+        ''' 查询一条数据 '''
+        return self.db.blog.find_one()
+
+    def get_more(self):
+        ''' 查询多条数据 '''
+        return self.db.blog.find({'x': None})  # 查询blog里面x没有值的数据
+
+    def get_one_from_oid(self, oid):
+        ''' 查询指定id的数据 '''
+        return self.db.blog.find_one({'_id': ObjectId(oid)})
+
 
 def main():
     obj = TestMongo()
-    rest = obj.add_one()
-    print(rest.inserted_id)
+    # rest = obj.add_one()
+    # print(rest.inserted_id)
+
+    # rest = obj.get_one()
+    # print(rest)
+
+    # rest = obj.get_more()
+    # for item in rest:
+    #     print(item['_id'])
+
+    rest = obj.get_one_from_oid('5e5ef9ee71d06d8f73481bc2')
+    print(rest)
 
 
 if __name__ == '__main__':
