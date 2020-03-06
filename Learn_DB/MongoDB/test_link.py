57414c60411b1463294392fc4306e85aa63ff779
KokoroWJY <oumainoli@gmail.com>
2020-03-06 18:05:53 +0800

    Learn MongoDB -- MongoDB链接

--- Learn_DB/MongoDB/test_link.py
+++ Learn_DB/MongoDB/test_link.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from mongoengine import connect  # 使用ODM连接MongoDB
 
 # 链接MongoDB的方法
 client = MongoClient()
@@ -6,3 +7,8 @@ client2 = MongoClient('localhost', 27017)
 client3 = MongoClient('mongodb://localhost:27017')
 
 print(client3.database_names())
+
+# 使用ODM连接MongoDB
+con = connect('students')
+con2 = connect('students', host='127.0.0.1', port=27017)
+con3 = connect('students', host='mongodb://127.0.0.1:27017/students')
