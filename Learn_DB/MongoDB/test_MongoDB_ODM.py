81390c8a06709fd9f795fcf45052e4790fd3baaf
KokoroWJY <oumainoli@gmail.com>
2020-03-06 18:05:41 +0800

    Learn MongoDB -- MongoDB-ODM

---
+++ Learn_DB/MongoDB/test_MongoDB_ODM.py
@@ -0,0 +1,25 @@
+from mongoengine import connect, Document, EmbeddedDocument, \
+    StringField, IntField, FloatField, ListField, EmbeddedDocumentField
+
+connect('students')
+
+SEX_CHOICES = (
+    ('male', '男'),
+    ('female', 'nv'),
+)
+
+
+class Grade(EmbeddedDocument):
+    ''' 成绩 '''
+    grade = StringField(required=True)
+    score = FloatField(required=True)
+
+
+class Student(Document):
+    ''' 学生 '''
+    name = StringField(max_length=32, required=True)
+    age = IntField(required=True)
+    grade = FloatField()
+    sex = StringField()
+    address = StringField()
+    grades = ListField(EmbeddedDocumentField(Grade))
