c471426a65c89bab49258ab03de1b1b5b2ad62da
KokoroWJY <oumainoli@gmail.com>
2020-03-12 08:40:14 +0800

    Learn MongoDB -- ODM 增加,查找, 更新, 删除

--- Learn_DB/MongoDB/test_MongoDB_ODM.py
+++ Learn_DB/MongoDB/test_MongoDB_ODM.py
@@ -53,10 +53,27 @@ class TestMongoEngine(object):
         ''' 使用ID来获取数据 '''
         return Student.objects.filter(id=oid).first()
 
-    def update(self):
-        """ 修改(更新)数据 """
+    def update_one(self):
+        """ 修改(更新)一条数据 """
+        return Student.objects.filter(sex='male').update_one(inc__age=100)
+
+    def update_more(self):
+        """ 修改(更新)多条数据 """
         # 修改所有男生的年龄增加10
+        # $gt:大于
+        # $lt:小于
+        # $gte:大于或等于
+        # $lte:小于或等于
+        # age__gt=16: 选择年龄大于16岁的
         return Student.objects.filter(sex='male').update(inc__age=10)
+
+    def delete(self):
+        """ 删除数据 """
+        # 删除一条数据
+        # return Student.objects.filter(age=130).first().delete()
+
+        # 删除多条数据
+        return Student.objects.filter(sex='male').delete()
 
 
 def main():
@@ -75,6 +92,12 @@ def main():
     # rest = obj.get_form_oid('5e6734c8a85e900657edc70c')
     # print(rest.id)
     # print(rest.name)
+
+    # rest = obj.update_one()
+    # print(rest)
+
+    rest = obj.delete()
+    print(rest)
 
 
 if __name__ == '__main__':
