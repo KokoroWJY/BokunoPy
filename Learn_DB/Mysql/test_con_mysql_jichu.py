380dbbcdd3060a36439c4b53819148a8854a37bd
KokoroWJY <oumainoli@gmail.com>
2020-03-12 08:40:51 +0800

    Learn MySQL-- 控制MySQL

--- Learn_DB/Mysql/test_con_mysql_jichu.py
+++ Learn_DB/Mysql/test_con_mysql_jichu.py
@@ -1,7 +1,8 @@
 import MySQLdb
 
 try:
-    conn = MySQLdb.connect(host='127.0.0.1', user='root', password='68dxqiji', db='news', port=3306, charset='utf8')
+    conn = MySQLdb.connect(host='127.0.0.1', user='root', password='68dxqiji',
+                           db='news', port=3306, charset='utf8')
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM `news`;")
     rest = cursor.fetchall()
